2b7026f0832568db99b248540dbec2f5549aaf8a
vijaymohire <vijaymohire@gmail.com>
2024-01-28 05:52:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {  
+    return {
+        name: 'Member',
+        skills: [
+            {
+                name: 'Member Skill 1',
+                description: 'Member Skill 1 Description'
+            },
+            {
+                name: 'Member Skill 2',
+                description: 'Member Skill 2 Description'
+            },
+            {
+                name: 'Member Skill 3',
+                description: 'Member Skill 3 Description'
+            }
+        ]
+    } 
+
+}
